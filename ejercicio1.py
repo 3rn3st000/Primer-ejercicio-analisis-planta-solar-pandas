d43248dadcfd25d1ba3b07b6f4f79145ec6e34bf
Ernesto <rodrigueznovalbos@gmail.com>
2026-04-09 09:22:36 +0200

    Enhance data analysis with visualizations
    Added data visualization using Seaborn and Matplotlib to analyze DC power generation and its correlation with solar irradiation.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,8 @@
 # %%
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 datosGeneracion = pd.read_csv(r"/home/ernesto/trabajo/Datasets/datos/solar/Plant_1_Generation_Data.csv")
 datosMeteorologia = pd.read_csv(r"/home/ernesto/trabajo/Datasets/datos/solar/Plant_1_Weather_Sensor_Data.csv")
 
@@ -45,7 +47,7 @@ def fueraRango(valor, min_val, max_val):
         
 datosUnidos['DC_POWER'] = datosUnidos['DC_POWER'].apply(
     fueraRango, 
-    min_val=0, 
+    min_val=1, 
     max_val=15000
 )
 
@@ -61,15 +63,31 @@ print(ranking_tramos)
 # Buscamos las que más y menos generan
 
 # %%
-datosUnidos.groupby('SOURCE_KEY_y')['DC_POWER'].mean().sort_values(ascending=False)
+
+dU_promedio = datosUnidos.groupby('SOURCE_KEY_y')['DC_POWER'].mean().sort_values(ascending=False).reset_index()
+sns.set_theme(style="whitegrid") 
+plt.figure(figsize=(12, 6))
+plot = sns.barplot(
+    data=dU_promedio, 
+    x='SOURCE_KEY_y', 
+    y='DC_POWER', 
+)
+plt.title('Promedio de DC POWER por Fuente (Inversor)', fontsize=16, fontweight='bold', pad=20)
+plt.xlabel('Source Key (Inversor)', fontsize=12)
+plt.ylabel('Promedio DC Power (kW)', fontsize=12)
+plt.xticks(rotation=45, ha='right')
+sns.despine(left=True, bottom=True)
+
+plt.tight_layout() 
+plt.show()
 
 # %% [markdown]
 # Ahora vamos a ver la relación entre la producción y la irradiación solar
 
 # %%
 correlacionIrradiacion = datosUnidos['DC_POWER'].corr(datosUnidos['IRRADIATION'])
-
-print (correlacionIrradiacion)
+sns.regplot(data=datosUnidos,x='DC_POWER',y= 'IRRADIATION',scatter_kws={'alpha':0.3},line_kws={'color':'red'})
+print (f"La correlación entre las dos variables es de {correlacionIrradiacion} lo que indica alta correlación")
 
 # %% [markdown]
 # ¿Qué es más importante para la generación, la temperatura del modulo, la ambiente o la irradiación?
@@ -88,5 +106,7 @@ lista_corr.sort(key=lambda x: x[1], reverse=True)
 
 for nombre, valor in lista_corr:
     print(f"{nombre} : {valor:.4f}")
+df_LCorr = pd.DataFrame(lista_corr,columns=['VARIABLE','COEFICIENTE'])
+sns.barplot(data=df_LCorr,x='COEFICIENTE',y='VARIABLE')
 
 
